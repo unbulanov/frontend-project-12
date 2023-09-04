36a2891512ccf026dc486ef0535033cc3a94d877
Ekaterina Ivanova <unbulanov@gmail.com>
2023-09-05 01:11:34 +0300

    fix bugs

--- frontend/src/Contexts/AuthContext.jsx
+++ frontend/src/Contexts/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from 'react';
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('userId'));
